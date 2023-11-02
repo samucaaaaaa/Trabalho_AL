8c7708266508ae348ede241306f216c103331ffe
samucaaaaaa <mucaspeedpvp@gmail.com>
2023-11-02 18:05:16 -0300

    Atualizando arquivos

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
 import streamlit as st
 
-st.set_page_config(page_title="Trabalho de Álgebra Linear")
-
-with st.container():
-    st.subheader("Meu primeiro site com o Streamlit")
-    st.title("Trasformações Lineares com imagens")
-    st.write("Aqui estão algumas transformações que podemos fazer com as imagens")
-    st.write("Quer acessar o projeto pelo GitHub? [Clique aqui](https://github.com/samucaaaaaa/Trabalho_AL)")
-
-with st.container():
-    st.write("---")
+st.title("Página Principal")
