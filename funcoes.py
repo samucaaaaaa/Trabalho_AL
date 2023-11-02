bfecbcc3e16a1efab62067e30ec4b259aefda7ba
Kaiky Braga <126474113+KaikyBraga@users.noreply.github.com>
2023-11-02 15:56:58 -0300

    Função escurecer imagem

--- funcoes.py
+++ funcoes.py
@@ -1,1 +1,12 @@
+import numpy as np
+from PIL import Image
 
+def escurecer_imagem(caminho_imagem, pct_escurecimento):
+    imagem = Image.open(caminho_imagem)
+    imagem_array = np.array(imagem)
+
+    for canal in range(0,3):
+        imagem_array[:,:,canal] = imagem_array[:,:,canal] * (100-pct_escurecimento)/100
+
+    resultado = Image.fromarray(imagem_array)
+    return resultado.save("imagem_resultado.png")
