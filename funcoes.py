90eee0391a3a4b54f5ba88963cbc6aed275d16e9
Kaiky Braga <126474113+KaikyBraga@users.noreply.github.com>
2023-11-02 15:57:27 -0300

    Alterações funções

--- funcoes.py
+++ funcoes.py
@@ -5,8 +5,8 @@ def escurecer_imagem(caminho_imagem, pct_escurecimento):
     imagem = Image.open(caminho_imagem)
     imagem_array = np.array(imagem)
 
-    for canal in range(0,3):
-        imagem_array[:,:,canal] = imagem_array[:,:,canal] * (100-pct_escurecimento)/100
+    for canal_cor in range(0,3):
+        imagem_array[:,:,canal_cor] = imagem_array[:,:,canal_cor] * (100-pct_escurecimento)/100
 
     resultado = Image.fromarray(imagem_array)
     return resultado.save("imagem_resultado.png")
