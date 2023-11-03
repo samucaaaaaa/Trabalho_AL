f9f5f29366a33cb9af54d37e7f074605d12a23c7
KaikyBraga <kaiky.braga06221607@gmail.com>
2023-11-03 16:22:16 -0300

    Motivação do traalho

---
+++ pages/motivacao.py
@@ -0,0 +1,54 @@
+import streamlit as st
+from PIL import Image, ImageDraw, ImageOps
+
+st.set_page_config(page_title="Motivação")
+
+with open("styles.css") as f:
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True) 
+
+st.title("Motivação")
+
+# Carregar as imagens
+kaiky = Image.open("kaiky.jpg")
+guilherme = Image.open("yuri.png")
+samuel = Image.open("samuel.jpg")
+
+# Tamanho desejado para as imagens redondas
+tamanho_redondo = (1000, 1000)
+
+# Criar máscaras circulares
+def aplicar_mascara(imagem, mascara):
+    imagem = ImageOps.fit(imagem, mascara.size, centering=(0.5, 0.5))
+    imagem.putalpha(mascara)
+    return imagem
+
+mascara1 = Image.new("L", tamanho_redondo, 0)
+draw1 = ImageDraw.Draw(mascara1)
+draw1.ellipse((0, 0) + tamanho_redondo, fill=255)
+
+mascara2 = Image.new("L", tamanho_redondo, 0)
+draw2 = ImageDraw.Draw(mascara2)
+draw2.ellipse((0, 0) + tamanho_redondo, fill=255)
+
+mascara3 = Image.new("L", tamanho_redondo, 0)
+draw3 = ImageDraw.Draw(mascara3)
+draw3.ellipse((0, 0) + tamanho_redondo, fill=255)
+
+kaiky = aplicar_mascara(kaiky, mascara1)
+guilherme = aplicar_mascara(guilherme, mascara2)
+samuel = aplicar_mascara(samuel, mascara3)
+
+# Adicionar margens transparentes às imagens para criar espaço entre elas
+kaiky = ImageOps.expand(kaiky, border=30, fill=None)
+guilherme = ImageOps.expand(guilherme, border=30, fill=None)
+samuel = ImageOps.expand(samuel, border=30, fill=None)
+
+# Exibir imagens redondas lado a lado com espaçamento e nomes abaixo delas
+st.image([kaiky, guilherme, samuel], width=230, caption=['Kaiky Braga', 'Guilherme Castilho', 'Samuel Lima'])
+
+st.write("Olá! Somos alunos do 2° Período de Ciência de Dados da Escola de Matemática Aplicada da Fundação Getúlio Vargas, iniciamos este projeto com o objetivo de aplicar os conceitos de Álgebra Linear de forma prática, explorando aplicações de transformações lineares e propriedades de matrizes, tudo isso combinado com a versatilidade da programação em Python e a interatividade proporcionada pela biblioteca Streamlit.")
+
+with st.container():
+    if st.button("Clique para acessar o projeto no GitHub :)"):
+        site_url = "https://github.com/samucaaaaaa/Trabalho_AL"
+        st.write(f'<meta http-equiv="refresh" content="0; url={site_url}" />', unsafe_allow_html=True)
