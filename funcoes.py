21342e16ae44b790ec55b9cc2fc7a4382293a413
GuilhermeCastilho02 <guilherme222castilho@gmail.com>
2023-11-09 15:08:43 -0300

    adequei as funções a nova forma

--- funcoes.py
+++ funcoes.py
@@ -3,8 +3,6 @@ from PIL import Image
 import cv2
 
 def escurecer_imagem(imagem_array, pct_escurecimento, escolhe):
-    # imagem = Image.open(caminho_imagem)
-    # imagem_array = np.array(imagem)
 
     for canal_cor in range(0,3):
         imagem_array[:,:,canal_cor] = imagem_array[:,:,canal_cor] * ((100-pct_escurecimento)/100)
@@ -15,9 +13,7 @@ def escurecer_imagem(imagem_array, pct_escurecimento, escolhe):
     elif escolhe == "dont show":
         return Image.fromarray(imagem_array)
 
-def imagem_preto_branco(caminho_imagem, escolhe):
-    imagem = Image.open(caminho_imagem)
-    imagem_array = np.array(imagem)
+def imagem_preto_branco(imagem_array, escolhe):
     
     imagem_array = np.dot(imagem_array[..., :3], [0.3, 0.59, 0.11])
 
@@ -27,17 +23,13 @@ def imagem_preto_branco(caminho_imagem, escolhe):
     elif escolhe == "dont show":
         return Image.fromarray(imagem_array)
 
-def rotacior_90(caminho_imagem):
-    imagem = Image.open(caminho_imagem)
-    imagem_array = np.array(imagem)
+def rotacior_90(imagem_array):
 
     imagem_array = np.transpose(imagem_array, (1,0,2))
     resultado = Image.fromarray(imagem_array)
     return resultado.save("imagem_resultado.png")
 
-def filtro_quente(caminho_imagem):
-    imagem = Image.open(caminho_imagem)
-    imagem_array = np.array(imagem)
+def filtro_quente(imagem_array):
 
     imagem_array[:,:,0] = np.clip(imagem_array[:,:,0] + 30, 0, 255).astype(np.uint8)
     imagem_array[:,:,1] = np.clip(imagem_array[:,:,1] + -10, 0, 255).astype(np.uint8)
@@ -49,10 +41,7 @@ def filtro_quente(caminho_imagem):
     resultado = Image.fromarray(imagem_array)
     return resultado.save("imagem_resultado.png")
 
-def inverte_imagem(caminho_imagem):
-    
-    imagem = Image.open(caminho_imagem)
-    imagem_array = np.array(imagem)
+def inverte_imagem(imagem_array):
     
     imagem_invertida_array = np.flip(imagem_array, 1)
 
@@ -60,18 +49,15 @@ def inverte_imagem(caminho_imagem):
 
     return imagem_invertida.save("imagem_resultado.png")
 
-def filtro_blur(caminho_imagem):
-    imagem = cv2.imread(caminho_imagem)
+def filtro_blur(caminho_da_imagem):
+    imagem = cv2.imread(caminho_da_imagem)
     matriz_filtro = np.ones((3, 3)) / 9
 
     imagem_borrada = cv2.filter2D(imagem, -1, matriz_filtro)
 
     return cv2.imwrite("imagem_resulatado.jpg", imagem_borrada)
 
-def repete_imagem(caminho_imagem, num_repeticoes):
-    imagem = Image.open(caminho_imagem)
-    
-    imagem_array = np.array(imagem)
+def repete_imagem(imagem_array, num_repeticoes):
 
     imagem_array = imagem_array[:, :, :3]
 
@@ -80,11 +66,7 @@ def repete_imagem(caminho_imagem, num_repeticoes):
 
     return imagem_repeticoes.save("imagem_resultado.png")
 
-def filtro_frio(caminho_imagem):
-    imagem = Image.open(caminho_imagem)
-    imagem_array = np.array(imagem)
-
-    imagem_array = np.array(imagem_array)
+def filtro_frio(imagem_array):
 
     imagem_array[:,:,2] = np.clip(imagem_array[:,:,2] * 1.2, 0, 255)
     imagem_array[:,:,0] = np.clip(imagem_array[:,:,0] * 0.8, 0, 255)
@@ -93,9 +75,9 @@ def filtro_frio(caminho_imagem):
 
     return imagem_filtrada.save("imagem_resultado.png")
 
-def redimensionar_imagem(caminho_imagem, altura, largura):
+def redimensionar_imagem(caminho_da_imagem, altura, largura):
     # Carrega a imagem do caminho especificado
-    imagem = cv2.imread(caminho_imagem)
+    imagem = cv2.imread(caminho_da_imagem)
 
     if imagem is None:
         print("Não foi possível carregar a imagem. Verifique o caminho.")
@@ -113,7 +95,8 @@ def redimensionar_imagem(caminho_imagem, altura, largura):
 
     return imagem_redimensionada
 
-def filtro_cor(caminho_da_imagem, cor):
+def filtro_cor(imagem_array, cor):
+    
     if cor.lower() == "vermelho":
         proporcoes = [1.5,1,1]
     elif cor.lower() == "verde":
@@ -127,9 +110,6 @@ def filtro_cor(caminho_da_imagem, cor):
     elif cor.lower() == "agua":
         proporcoes = [1,1.5,1.5]
 
-    imagem = Image.open(caminho_da_imagem)
-    imagem_array = np.array(imagem)
-
     filtro = np.array(proporcoes)
     imagem_transformada = imagem_array * filtro
 
@@ -139,9 +119,7 @@ def filtro_cor(caminho_da_imagem, cor):
 
     return resultado.save("imagem_resultado.png")
 
-def filtro_sepia(caminho_imagem):
-    imagem = Image.open(caminho_imagem)
-    imagem_array = np.array(imagem)
+def filtro_sepia(imagem_array):
     
     sepia_filter = np.array([[.393, .769, .189],
                             [.349, .686, .168],
@@ -155,9 +133,7 @@ def filtro_sepia(caminho_imagem):
 
     return resultado.save("imagem_resultado.png")
 
-def cor_negativa(caminho_imagem):
-    imagem = Image.open(caminho_imagem)
-    imagem_array = np.array(imagem)
+def cor_negativa(imagem_array):
 
     for canal_cor in range(0,3):
         imagem_array[:,:,canal_cor] = 255 - imagem_array[:,:,canal_cor]
