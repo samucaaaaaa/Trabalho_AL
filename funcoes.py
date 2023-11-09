bc4e4ef24675e3aff4ce24a6d0bd2da95bd32381
samucaaaaaa <mucaspeedpvp@gmail.com>
2023-11-09 12:44:51 -0300

    Criando novo botão e arrumando funções

--- funcoes.py
+++ funcoes.py
@@ -2,23 +2,30 @@ import numpy as np
 from PIL import Image
 import cv2
 
-def escurecer_imagem(caminho_imagem, pct_escurecimento):
+def escurecer_imagem(caminho_imagem, pct_escurecimento, escolhe):
     imagem = Image.open(caminho_imagem)
     imagem_array = np.array(imagem)
 
     for canal_cor in range(0,3):
         imagem_array[:,:,canal_cor] = imagem_array[:,:,canal_cor] * ((100-pct_escurecimento)/100)
 
-    return Image.fromarray(imagem_array)
+    if escolhe == "show":
+        resultado = Image.fromarray(imagem_array)
+        return resultado.show("imagem_resultado.png")
+    elif escolhe == "dont show":
+        return Image.fromarray(imagem_array)
 
-def imagem_preto_branco(caminho_imagem):
+def imagem_preto_branco(caminho_imagem, escolhe):
     imagem = Image.open(caminho_imagem)
     imagem_array = np.array(imagem)
     
     imagem_array = np.dot(imagem_array[..., :3], [0.3, 0.59, 0.11])
 
-    resultado = Image.fromarray(imagem_array)
-    return resultado.save("imagem_resultado.png")
+    if escolhe == "show":
+        resultado = Image.fromarray(imagem_array)
+        return resultado.show("imagem_resultado.png")
+    elif escolhe == "dont show":
+        return Image.fromarray(imagem_array)
 
 def rotacior_90(caminho_imagem):
     imagem = Image.open(caminho_imagem)
--- pages/index.py
+++ pages/index.py
@@ -1,6 +1,6 @@
 from PIL import Image
 import streamlit as st
-from funcoes import escurecer_imagem
+from funcoes import *
 
 st.set_page_config(page_title="Trabalho de Álgebra Linear")
 
@@ -20,25 +20,32 @@ with st.container():
     st.write("---")
     st.write("Clique no botão para alterar a imagem!")
     st.image("imagens/yuri.png") 
+
+    # Botão escure imagem
     if st.button("Escurecer Imagem"):
-        escurecer_imagem("imagens/yuri.png", 70)
+        escurecer_imagem("imagens/yuri.png", 70, "show")
+
+    # Botão imagem preto e branco
+    if st.button("Imagem preto e branco"):
+        imagem_preto_branco("imagens/yuri.png", "show")    
 
 
-# Usa st.file_uploader para permitir o upload de imagens
-uploaded_file = st.file_uploader("Faça o upload de uma imagem", type=["jpg", "png", "jpeg"])
 
-# Verifica se um arquivo foi enviado
-if uploaded_file is not None:
-    # Carrega a imagem original
-    imagem_original = Image.open(uploaded_file)
+with st.container():
+    # Usa st.file_uploader para permitir o upload de imagens
+    uploaded_file = st.file_uploader("Faça o upload de uma imagem", type=["jpg", "png", "jpeg"])
+    # Verifica se um arquivo foi enviado
+    if uploaded_file is not None:
+        # Carrega a imagem original
+        imagem_original = Image.open(uploaded_file)
 
-    # Salva a imagem original em um arquivo temporário
-    caminho_da_imagem_original = "temp_imagem_original.png"
-    imagem_original.save(caminho_da_imagem_original, format="PNG")
-    st.image(imagem_original, caption="Imagem Original", use_column_width=True)
+        # Salva a imagem original em um arquivo temporário
+        caminho_da_imagem_original = "temp_imagem_original.png"
+        imagem_original.save(caminho_da_imagem_original, format="PNG")
+        st.image(imagem_original, caption="Imagem Original", use_column_width=True)
 
-    imagem_alterada = escurecer_imagem(caminho_da_imagem_original, 70)
-    caminho_da_imagem_alterada = "temp_imagem_alterada.png"
-    imagem_alterada.save(caminho_da_imagem_alterada, format="PNG")
-    st.image(imagem_alterada, caption="Imagem Alterada", use_column_width=True)
-    # TODO: Criar um botão para download, tirando o download automático
+        imagem_alterada = escurecer_imagem(caminho_da_imagem_original, 70, "dont show")
+        caminho_da_imagem_alterada = "temp_imagem_alterada.png"
+        imagem_alterada.save(caminho_da_imagem_alterada, format="PNG")
+        st.image(imagem_alterada, caption="Imagem Alterada", use_column_width=True)
+        # TODO: Criar um botão para download, tirando o download automático
