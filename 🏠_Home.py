59a087bffd69eb82e673bfb9c459f1f978bf74fd
KaikyBraga <kaiky.braga06221607@gmail.com>
2023-11-19 15:04:53 -0300

    Criação Página Inicial

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -5,4 +5,15 @@ st.set_page_config(page_title="Introdução")
 with open("styles.css") as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-st.title("Página Principal")
+st.title("Nome do Site🌟")
+
+st.subheader("Dê um toque estiloso às suas fotos com facilidade")
+st.write("Olá! Bem-vindo ao NOME_SITE, o lugar perfeito para editar as suas fotos! 📸")
+
+st.subheader("Autenticidade")
+st.write("Experimente as diversas opções de personalização ou teste as alterações com a imagem fornecida pelo aplicativo. 🙌")
+
+st.subheader("Salve suas criações com praticidade")
+st.write("Também facilitamos a opção de salvamento para você! Basta fazer o upload da sua imagem, clicar em salvar, confirmar o download e escolher o local preferido para armazenar. 📥")
+
+st.subheader("Aproveite ao máximo o aplicativo! 😁")
